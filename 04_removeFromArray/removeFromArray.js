0a3198c4634480e644915e24d4f8250f6f8fcf42
Leandro Silva <leandro.jj.silva@gmail.com>
2024-12-16 23:18:15 -0300

    Feature: exercise 4 solution using includes

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(arrayToBeRemoved, ...elements) {
+    let comparator;
+    const modifiedArray = arrayToBeRemoved.filter(
+        (item) => !elements.includes(item)   
+    );
+    
+    return modifiedArray;
 };
 
 // Do not edit below this line
