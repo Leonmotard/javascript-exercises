358f934f81f0b66ee7741362e802347c298a9353
Leandro Silva <leandro.jj.silva@gmail.com>
2024-12-18 20:10:10 -0300

    Feature: Exercise 7 solved

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temperature) {
+  // (x − 32) × ⁠5/9⁠ °C 
+  let temperatureConverted = (temperature - 32) * (5 / 9);
+  if (Number.isInteger(temperatureConverted)) {
+    return temperatureConverted;
+  } else return Math.round(temperatureConverted * 10) /10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temperature) {
+  //(x × ⁠9/5⁠ + 32) 
+  let temperatureConverted = (temperature * (9 / 5) + 32);
+  if (Number.isInteger(temperatureConverted)) {
+    return temperatureConverted;
+  } else return Math.round(temperatureConverted * 10) /10;
 };
 
 // Do not edit below this line
