916f65c1ae0833ced66ce4b45aea47492a014602
Leandro Silva <leandro.jj.silva@gmail.com>
2024-12-18 19:54:37 -0300

    Feature: Exercise 6 solved

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,10 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+    const condition = (year % 4 == 0 && year % 100 !=0) || year % 400 ==0; 
+
+    if(condition){
+            return true;
+    }else  return false;
 };
 
 // Do not edit below this line
